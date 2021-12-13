6609a5e2a9baf281b6ae4b004ff85433090f35c6
Ben Adam <badam@bens-air.lan>
2021-12-12 22:03:34 -0700

    Adding some stuff

--- src/components/text-video.jsx
+++ src/components/text-video.jsx
@@ -3,38 +3,44 @@ import { useState, useRef, useEffect } from "preact/hooks";
 import { createPortal } from "preact/compat";
 
 function Video({ show, coords }) {
-  const el = useRef(null)
-  useEffect(async ()=> {
-    
-    if(show) {
+  const el = useRef(null);
+  useEffect(async () => {
+    if (show) {
       // el.current.muted = false;
-      await el.current.play()
-
+      try {
+        await el?.current?.play();
+      } catch (e) {
+        console.log(e);
+      }
     } else {
       // el.current.muted = true;
-      el.current.pause()
+      el?.current?.pause();
     }
-  }, [show])
-  return show && (
-    <video
-      ref={el}
-      style={{ left: coords[0], top: coords[1] + 60}}
-      class={`fixed max-w-[600px] right-0 aspect-video ${show ? 'show' : ''}`}
-      src={video}
-    />
-  )
+  }, [show]);
+  return (
+    <div class="fixed" style={{ left: coords[0], top: coords[1] + 60 }}>
+      {show && (
+        <video
+        muted
+          ref={el}
+          class={`max-w-[600px] right-0 aspect-video ${show ? "show" : ""}`}
+          src={video}
+        />
+      )}
+    </div>
+  );
 }
 
 export default function TextVideoPopover({ children }) {
+  const [container, setContainer] = useState(null);
   const [show, setShow] = useState(false);
-  const [coords, setCoords] = useState([0,0]);
-  const el = useRef(null)
-  useEffect(()=> {
-    const {x,y} = el.current.getBoundingClientRect();
-    setCoords([x,y])
-  }, [])
-
-  console.log(coords)
+  const [coords, setCoords] = useState([0, 0]);
+  const el = useRef(null);
+  useEffect(() => {
+    const { x, y } = el.current.getBoundingClientRect();
+    setCoords([x, y]);
+    setContainer(document.body);
+  }, []);
 
   return (
     <span
@@ -44,7 +50,9 @@ export default function TextVideoPopover({ children }) {
       onMouseLeave={() => setShow(false)}
     >
       {children}
-      {createPortal(<Video show={show} coords={coords} />, document.body)}
+      {container
+        ? createPortal(<Video show={show} coords={coords} />, container)
+        : null}
     </span>
   );
 }
