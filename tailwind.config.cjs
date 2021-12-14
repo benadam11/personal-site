4c51d2c95bf8d2ba6fced25b9e1800c2e02ec185
Ben Adam <badam@bens-air.lan>
2021-12-13 20:23:32 -0700

    Update deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue}"],
+  content: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue}"],
   theme: {
     fontFamily: {
       sans: ["raleway", "system-ui"],
