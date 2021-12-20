f31aa4f463b4b2abe1d6bd4d71f7a2206f177b5f
benadam11 <benjamin.mark.adam@gmail.com>
2021-12-20 13:14:45 -0700

    Tweaks

--- src/components/particle-animation.jsx
+++ src/components/particle-animation.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useCallback } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as Slider from "@radix-ui/react-slider";
 import { animateCanvas } from "../scripts/particle-animation";
 
