be65f715c9a2126e351dd94f89fe1140f636ceea
Benjamin Adam <benjamin.mark.adam@gmail.com>
2024-09-30 09:32:31 -0700

    Tweak colors

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,5 +1,5 @@
 export default function Card() {
   return (
-    <div className="bg-shadow text-contrast w-[250px] aspect-[3/4] p-5 rounded-lg shadow-lg border-base border"></div>
+    <div className="bg-acid  text-contrast w-[250px] aspect-[3/4] p-5 rounded-lg shadow-lg border-2 border-[#000]"></div>
   );
 }
